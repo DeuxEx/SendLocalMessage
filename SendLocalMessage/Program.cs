9cdc3e0533f442709dc0e181a5ab26b639791f6a
Per Wennergrund <per@wennergrund.net>
2024-03-14 13:27:47 +0100

    Add project files.

---
+++ SendLocalMessage/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SendLocalMessage
+{
+    class Program
+    {
+
+
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            try
+            {
+                QuestingWindow.Main();
+
+                //UserControl1 userControl1 = new UserControl1();
+
+            }
+            catch (Exception e) 
+            { 
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ SendLocalMessage/QuestingWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+
+namespace SendLocalMessage
+{
+    internal class QuestingWindow
+    {
+        public static Window window;
+
+
+        public static void Main()
+        {
+
+            window = new Window
+            {
+                Content = new UserControl1(),
+                Height = UserControl1.AppWindow.Height,
+                Width = UserControl1.AppWindow.Width,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                Topmost = true
+            };
+            window.ShowDialog();
+        }
+    }
+}
